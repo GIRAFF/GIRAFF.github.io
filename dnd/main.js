0cbaab1fde31a0aada1b735b5304600f3da46895
GIRAFF <giraff.giraffov@gmail.com>
2015-06-20 00:19:48 +0600

    onclick works!
    Signed-off-by: GIRAFF <giraff.giraffov@gmail.com>

--- dnd/main.js
+++ dnd/main.js
@@ -1,8 +1,3 @@
-function main()
-{
-	
-}
-
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload('./img/background.jpg');
@@ -11,7 +6,18 @@ ASSET_MANAGER.queueDownload('./img/background.jpg');
 ASSET_MANAGER.downloadAll(function()
 {
 	$("#overlay").fadeOut('slow');
-	main();
+});
+
+$(document).on('click', '[data-description]', function(e)
+{
+	$head = $(this).data("head");
+	$description = $(this).data("description");
+
+	$("#info_box > #head").text($head);
+	$("#info_box > #description").text($description);
+	$("#info_box").css("top", e.pageY + 5)
+				.css("left", e.pageX + 5)
+				.css("display", "inline");
 });
 
 /*$("path").mouseenter(function()
