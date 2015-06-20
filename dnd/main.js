334337c8cf9f309b734802815b3461afbd8bfe45
GIRAFF <giraff.giraffov@gmail.com>
2015-06-21 00:58:20 +0600

    info_box now fades out when empty space clilcked. Awsome animation added.
    Signed-off-by: GIRAFF <giraff.giraffov@gmail.com>

--- dnd/main.js
+++ dnd/main.js
@@ -1,17 +1,23 @@
 var ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload('./img/background.jpg');
-//ASSET_MANAGER.queueDownload('img/names.png');
+ASSET_MANAGER.queueDownload("./img/background.jpg");
 
 ASSET_MANAGER.downloadAll(function()
 {
-	$("#overlay").fadeOut('slow');
+	$("#overlay").fadeOut("slow");
 });
 
-$(document).on('click', '[data-description]', function(e)
+$(document).on("click", "svg", function(e)
+{
+	$info_box = $("#info_box");
+	if($info_box.is(":visible") && !$(e.target).is("path"))
+		$info_box.fadeOut("slow");
+});
+
+$(document).on("click", "[data-description]", function(e)
 {
 	var
-		$info_box = $("#info_box");
+		$info_box = $("#info_box"),
 		head = $(this).data("head"),
 		description = $(this).data("description"),
 		w = $(window).width(),
@@ -22,20 +28,25 @@ $(document).on('click', '[data-description]', function(e)
 	
 	// выравнивание
 	if(e.pageX <= w/2)
-		$info_box
-			.css("left", e.pageX)
-			.css("right", "auto");
+		$info_box.css({
+			"left": e.pageX,
+			"right": "auto"
+		});
 	else
-		$info_box
-			.css("left", "auto")
-			.css("right", w - e.pageX);
+		$info_box.css({
+			"left": "auto",
+			"right": w - e.pageX
+		});
 	if(e.pageY <= h/2)
-		$info_box
-			.css("top", e.pageY)
-			.css("bottom", "auto");
+		$info_box.css({
+			"top": e.pageY,
+			"bottom": "auto"
+		});
 	else
-		$info_box
-			.css("top", "auto")
-			.css("bottom", h - e.pageY);
-	$info_box.css("display", "inline");
+		$info_box.css({
+			"top": "auto",
+			"bottom": h - e.pageY
+		});
+	//$info_box.css("display", "inline");
+	$info_box.show("fast");
 });
